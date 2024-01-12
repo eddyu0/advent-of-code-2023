d95503635c9394a4997fce931e960593fe2b1e10
Eddie Yu <32135564+eddieyutc@users.noreply.github.com>
2024-01-12 08:06:17 +0000

    Day 15: TS part 1

---
+++ typescript/day15/part1.ts
@@ -0,0 +1,36 @@
+import { readFileSync } from "fs";
+import path from "path";
+import { sum } from "../util";
+
+function hash(input: string): number {
+  let result = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    result += input[i].charCodeAt(0);
+    result *= 17;
+    result %= 256;
+  }
+
+  return result;
+}
+
+export function solve(input: string): number {
+  const inputs = input.split(",");
+
+  const result = sum(inputs.map(hash));
+
+  return result;
+}
+
+function main() {
+  const input = readFileSync(path.join(__dirname, "./input.txt"), {
+    encoding: "utf-8",
+  });
+
+  const result = solve(input);
+  console.log(result);
+}
+
+if (require.main === module) {
+  main();
+}
